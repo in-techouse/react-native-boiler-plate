979157772d1138e094021bc11241562044137396
hth-developers <talhamanzoor006@gmail.com>
2020-09-23 20:03:45 +0500

    Added One Signal Push Notification & firebase attached

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-
+import Notification from './Notification';
+// e3a0ed24-b63d-4550-ba7b-63093fb04ad2 One Signal App iD
 const HomeScreen = () => {
   return (
     <View>
+      <Notification />
       <Text> HOME</Text>
     </View>
   );
